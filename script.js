a3d6a29a3ef2bd4b3ade55e4ac75af28209afc64
unknown <romanyefimets@gmail.com>
2022-02-16 15:57:41 -0800

    modified checking of feilds to be more modular to reduce code

--- script.js
+++ script.js
@@ -26,33 +26,24 @@ function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
 }
+
+// check required feilds
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 // Event listener
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-
-  if (username.value === '') {
-    showError(username, 'Username is required');
-  } else {
-    showSuccess(username);
-  }
-
-  if (email.value === '') {
-    showError(email, 'Email is required');
-  } else if (!isEmailValid(email.value)) {
-    showError(email, 'Email is not valid');
-  } else {
-    showSuccess(email);
-  }
-
-  if (password.value === '') {
-    showError(password, 'Password is required');
-  } else {
-    showSuccess(password);
-  }
-
-  if (password2.value === '') {
-    showError(password2, 'password 2 is required');
-  } else {
-    showSuccess(password2);
-  }
+  checkRequired([username, email, password, password2]);
 });
